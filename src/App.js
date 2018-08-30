e16a3b24d188e5abc41b9e32243963edbf77c233
esolar07 <esolar07@gmail.com>
2018-08-30 08:10:36 -0400

    working on issue with router

--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
-import React, { Component } from 'react';
-// import logo from './logo.svg';
-import {
-  BrowserRouter,
-  Route
-} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
+import Header from './components/Header'
 import Home from './containers/Home'
-import Menu from './containers/Menu'
-import Schedule from './containers/Schedule'
-import Events from './containers/Events'
-import News from './containers/News'
-import Contact from './containers/Contact'
+// import Menu  from './containers/Menu'
+// import Schedule from './containers/Schedule'
+// import Events from './containers/Events'
+// import News from './containers/News'
+// import Contact from './containers/Contact'
 
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <Route exact path='/' component={Home} />
-          <Route path='/menu' component={Menu} />
-          <Route path='/schedule' component={Schedule} />
-          <Route path='/events' component={Events} />
-          <Route path='/news' component={News} />
-          <Route path='/contact' component={Contact} />
-        </div>
-      </BrowserRouter>
-    );
-  }
-}
+const App= () => (
+  <Router>
+    <div className="App">
+      <Header />
+      <Route exact path='/' render={() => <Home />} />
+      {/* <Route path='/menu' render={ () => <Menu />} />
+      <Route path='/schedule' render={() => <Schedule />} />
+      <Route path='/events' render={() => <Events />} />
+      <Route path='/news' render={() => <News />} />
+      <Route path='/contact' render={() => <Contact />} /> */}
+    </div>
+  </Router>
+);
+
 
 export default App;
